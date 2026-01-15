9b45e29b3e9d3f17925c13d3888c984a90b3777c
Daniel23894 <daso0005@stud.kea.dk>
2026-01-15 14:25:04 +0100

    Setup: Samlet repository til backend og frontend

---
+++ backend/beskyttelsesrum-eksamen/src/main/java/org/example/beskyttelsesrumeksamen/BeskyttelsesrumEksamenApplication.java
@@ -0,0 +1,13 @@
+package org.example.beskyttelsesrumeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeskyttelsesrumEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeskyttelsesrumEksamenApplication.class, args);
+    }
+
+}
---
+++ backend/beskyttelsesrum-eksamen/src/test/java/org/example/beskyttelsesrumeksamen/BeskyttelsesrumEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.beskyttelsesrumeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeskyttelsesrumEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
